ff2c23c4d0deec5ef544e0ad05e495c55348f129
Favour Ukonu <favouroukonu@gmail.com>
2023-08-27 21:12:04 +0000

    initial-changes

--- src/assignment.js
+++ src/assignment.js
@@ -16,11 +16,19 @@ const assignment = {};
  */
 function sumOfNumbersTo(destination) {
     let sum = 0;
+
+    for(let i = 0; i <= destination; i++){
+        sum += i
+    }
+    
+    
     return sum
 }
 
+sumOfNumbersTo(10);
 
-// assignment.sumOfNumbersTo = sumOfNumbersTo;
+assignment.sumOfNumbersTo = sumOfNumbersTo;
+
 
 /**
  * Challenge - 2
@@ -39,6 +47,18 @@ function countEvenNumbersWithin(destination) {
     let count = 0;
     let arrayOfEvenNumbers = [];
 
+    for(let i= 1; i <= destination; i++){
+    if(i % 2 === 0) {
+        arrayOfEvenNumbers.push(i);
+        count ++;
+        sum += i;
+    } 
+}
+
+       
+    
+
+
     return {
         // property value shorthand
         // when the property name and the value name are the same
@@ -49,7 +69,7 @@ function countEvenNumbersWithin(destination) {
     };
 }
 
-// assignment.countEvenNumbersWithin = countEvenNumbersWithin;
+ assignment.countEvenNumbersWithin = countEvenNumbersWithin;
 
 /**
  * Challenge - 3
